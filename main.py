09efa14bcafa882514ef565a386205023a1a1a8c
Kamil <kamil.suwiczak@wp.pl>
2024-10-21 17:59:13 +0200

    created the beginning of program

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello world!")
+
+
+pathfile1 = "map_points.txt"
+with open(pathfile1, "r") as file1:
+    data = file1.readlines()
+
+points = [[] for i in range(len(data))]
+print(points)
+#for i in range(len(data)):
+
+for i in range(len(data)):
+    data[i] = data[i].split()
+print(data)
+file1.close()
