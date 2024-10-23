b6cfcc58c646103cedeb363ebe93bb4361455d90
Kamil <kamil.suwiczak@wp.pl>
2024-10-23 14:57:23 +0200

    updated main.py to read from actual format of file

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 #reading points from file
 pathfile1 = "map_points.txt"
 with open(pathfile1, "r") as file1:
+    file1.readline()
     point_coordinates = file1.readlines()
+
 
 #converting points to list and to int
 for i in range(len(point_coordinates)):
     point_coordinates[i] = point_coordinates[i].rstrip()
-    point_coordinates[i] = point_coordinates[i].split(";")
+    point_coordinates[i] = point_coordinates[i].split(" ")
+    point_coordinates[i].pop(0)
     point_coordinates[i][0] = int(point_coordinates[i][0])
     point_coordinates[i][1] = int(point_coordinates[i][1])
 
