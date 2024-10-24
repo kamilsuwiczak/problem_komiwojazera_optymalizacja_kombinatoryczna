4c06e6697ae76499abcde26a816db2e763e1bc6a
Kamil <kamil.suwiczak@wp.pl>
2024-10-24 14:13:32 +0200

    added measuring time of execution

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-
+import time
 def distance(p1,p2):
     return ((p1[2]-p2[2])**2 + (p1[1]-p2[1])**2)**0.5
 
 #reading points from file
-pathfile1 = "map_points.txt"
+pathfile1 = "test.txt"
 with open(pathfile1, "r") as file1:
     #file1.readline()
     point_coordinates = file1.readlines()
+file1.close()
+
 point_coordinates[0]=[0,0,0]
 
 #converting points to list and to int
@@ -17,8 +19,12 @@ for i in range(1,len(point_coordinates)):
     point_coordinates[i][1] = int(point_coordinates[i][1])
     point_coordinates[i][2] = int(point_coordinates[i][2])
 
-print(point_coordinates)
-path = "0 -> "
+# print(point_coordinates)
+
+# measuring time of execution
+start_time = time.time()
+
+path = "0"
 length = 0
 current_point = point_coordinates[0]
 point_coordinates.pop(0)
@@ -29,11 +35,16 @@ for i in range(len(point_coordinates)):
         if distance(current_point,point) < min_distance:
             min_distance = distance(current_point,point)
             next_point = point
-    path += str(next_point[0]) + " -> "
+    path += " -> " + str(next_point[0])
     length += distance(current_point,next_point)
     current_point = next_point
     point_coordinates.remove(current_point)
-path += "0"
+
 length += distance(current_point,[0,0,0])
-print(path,length)
-file1.close()
+
+finish_time = time.time() - start_time
+
+print("Ścieżka: " + path)
+print("Długość ścieżki: " + str(length))
+print("Czas działania programu: " + str(finish_time))
+
